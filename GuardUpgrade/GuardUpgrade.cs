eacdfeaca445fc1e9f96e882112b4a4321d49869
Cyanox62 <infernogaming621@gmail.com>
2018-09-29 15:07:58 -0400

    Add files via upload

---
+++ GuardUpgrade/GuardUpgrade.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Smod2;
+using Smod2.API;
+using Smod2.Attributes;
+
+namespace GuardUpgrade
+{
+    public class GuardUpgrade
+    {
+    [PluginDetails(
+    author = "Cyanox",
+    name = "Guard Upgrade",
+    description = "Upgrades gaurds to MTF cadets",
+    id = "cyan.gaurd.upgrade",
+    version = "0.1",
+    SmodMajor = 3,
+    SmodMinor = 0,
+    SmodRevision = 0
+)]
+        public class GaurdUpgrade : Plugin
+        {
+            public override void OnDisable() { }
+
+            public override void OnEnable() { }
+
+            public override void Register()
+            {
+                this.AddEventHandlers(new RoundEventHandler(this));
+                this.AddConfig(new Smod2.Config.ConfigSetting("guard_upgrade", true, Smod2.Config.SettingType.BOOL, true, "Upgrades guard."));
+            }
+        }
+    }
+}
---
+++ GuardUpgrade/RoundEventHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Smod2;
+using Smod2.API;
+using Smod2.EventHandlers;
+using Smod2.Events;
+
+namespace GuardUpgrade
+{
+    class RoundEventHandler : IEventHandlerCheckEscape
+    {
+        private Plugin plugin;
+
+        public RoundEventHandler(Plugin plugin)
+        {
+            this.plugin = plugin;
+        }
+
+        public void OnCheckEscape(PlayerCheckEscapeEvent ev)
+        {
+            if (ev.Player.TeamRole.Role.Equals(Role.FACILITY_GUARD) && plugin.GetConfigBool("gaurd_upgrade"))
+            {
+                ev.Player.ChangeRole(Role.NTF_CADET);
+            }
+        }
+    }
+}
